bf742f7739b9eee0f3eea87fda0b21d34909c77a
Your Name <your_email@example.com>
2018-04-13 22:06:17 +0200

    sync perm modify to role modify

--- entities/Role.php
+++ entities/Role.php
@@ -16,14 +16,25 @@ while ($row = $result->fetch_assoc()) {
     array_push($roles, new Role($row));
 }
 
+foreach ($roles as $role) {
+    $roleid = $role->getId();
+    $sqlGetPerms = "SELECT perm_id FROM perm_role_connections WHERE role_id = '$roleid'";
+    $permResult = $conn->query($sqlGetPerms);
+    while ($row = $permResult->fetch_assoc()) {
+        $role->addPerm($row['perm_id']);
+    }
+}
+
 class Role {
 
     private $id;
     private $name;
+    private $perms;
 
     function __construct($row) {
         $this->id = $row['id'];
         $this->name = $row['name'];
+        $this->perms = [];
     }
 
     public function getId() {
@@ -34,4 +45,22 @@ class Role {
         return $this->name;
     }
 
+    public function getPerms() {
+        return $this->perms;
+    }
+
+    public function addPerm($perm) {
+        array_push($this->perms, $perm);
+    }
+
+}
+
+function getRoleFromId($id) {
+    global $roles;
+    foreach ($roles as $role) {
+        if ($id == $role->getId()) {
+            return $role;
+        }
+    }
+    return null;
 }
--- functions/updateRoles.php
+++ functions/updateRoles.php
@@ -7,19 +7,31 @@
  */
 
 require 'database.php';
+require '../entities/Role.php';
 global $conn;
 session_start();
 
 if (isset($_GET['userid'])) {
-    foreach (array_keys($_GET) as $role) {
-        if ($role != "user") {
+    foreach (array_keys($_GET) as $roleId) {
+        if ($roleId != "user") {
             $user = $_GET['userid'];
-            if ($_GET[$role] == "add") {
-                $insert = "INSERT INTO user_roles (role_id, user_id) VALUES ('$role', '$user')";
+            if ($_GET[$roleId] == "add") {
+                $insert = "INSERT INTO user_roles (role_id, user_id) VALUES ('$roleId', '$user')";
                 $conn->query($insert);
-            } elseif ($_GET[$role] == "remove") {
-                $delete = "DELETE FROM user_roles WHERE role_id = '$role' AND user_id = '$user'";
+                $role = getRoleFromId($roleId);
+                foreach ($role->getPerms() as $permId) {
+                    $insertPerm = "INSERT INTO user_perms (perm_id, user_id) VALUES ('$permId', '$user')";
+                    $conn->query($insertPerm);
+                }
+            } elseif ($_GET[$roleId] == "remove") {
+                $delete = "DELETE FROM user_roles WHERE role_id = '$roleId' AND user_id = '$user'";
                 $conn->query($delete);
+                $role = getRoleFromId($roleId);
+                foreach ($role->getPerms() as $permId) {
+                    $deletePerm = "DELETE FROM user_perms WHERE perm_id = '$permId' AND user_id = '$user'";
+                    $conn->query($deletePerm);
+
+                }
             }
         }
     }
