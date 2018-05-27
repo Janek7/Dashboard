ef36fecc095fa50d623a20fee524ddaa99a06ee3
Janek <janekputz@web.de>
2018-05-27 21:55:13 +0200

    test

--- index.php
+++ index.php
@@ -37,7 +37,6 @@ logLastActivity($page);
 
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-
     <!-- Google Font -->
     <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
